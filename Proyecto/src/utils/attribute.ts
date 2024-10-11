beff11e96a0014cdf840e1cff2462b60cdf893a9
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-10-11 11:53:20 -0500

    Elimina archivos innecesarios

--- Proyecto/src/utils/attribute.ts
+++
@@ -1,5 +0,0 @@
-export const setAttributes = <element extends Record<any, any>>(objProps: element, elementRef: HTMLElement) => {
-    Object.entries(objProps).forEach(
-        ([prop, value]) => elementRef.setAttribute(prop, value)
-    )
-}
